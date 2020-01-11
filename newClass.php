35d9e5b7af9244daa56215f9fe3824a43b86eba3
Nakib Hossin <hussain0296@gmail.com>
2020-01-12 04:55:33 +0600

    Static Operations and Methods

--- newClass.php
+++ newClass.php
@@ -51,4 +51,31 @@
             echo "Destroyed"; 
         }
     }
+    /* Stattic Properties and Methods. They can be directly called without creating any instances of the class */
+    class PersonY{
+        private $name;
+        private $gender;
+
+        public static $DrinkingAge = 18;
+        public function __construct($name,$age,$gender){
+            $this->name = $name;
+            $this->age = $age;
+            $this->gender = $gender;
+        }
+        public function showPersonInfo(){
+            $info = '<br>'.$this->name.'<br>'.$this->age.'<br>'.$this->gender.'<br><br>';
+            return $info;
+        }
+        //public function setDrinkingAge($age){ // This will give warning.
+        //    self::$DrinkingAge = $age;
+        //}
+        public static function setDrinkingAge($age){ // This will not give warning.
+            self::$DrinkingAge = $age;
+        }
+
+        /*We can also access static variable normally. (using object)*/
+        public function printStatic(){
+            return self::$DrinkingAge;
+        }
+    }
 ?>
