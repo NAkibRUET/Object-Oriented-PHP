88c7af44829e34e8c0b0daef6df186ecbecda58c
Nakib Hossin <hussain0296@gmail.com>
2020-01-12 04:16:38 +0600

    Constructor Destructor

--- index.php
+++ index.php
@@ -8,11 +8,15 @@
     <body>
         <div>Nakib Hossain</div>
         <?php
-            $obj = new Derived();
+            $obj = new Derived(); // The Paranthesis are not important here because there is no Contructor here
             echo $obj->showGender();
-            $p1 = new Person();
+        
+            $p1 = new Person(); // The Paranthesis are not important here because there is no Contructor here
             $p1->setPerson("Nakib",23,"Male");
             echo $p1->showPersonInfo();
+        
+            $p2 = new PersonX("Mr X", 24, "Male"); // Without Paranthesis it will give an erro, because constructor exists
+            echo $p2->showPersonInfo();
         ?>
     </body>
 </html>
--- newClass.php
+++ newClass.php
@@ -25,8 +25,30 @@
             $this->gender = $gender;
         }
         public function showPersonInfo(){
-            $info = $this->name.' '.$this->age.' '.$this->gender;
+            $info = '<br><br>'.$this->name.'<br>'.$this->age.'<br>'.$this->gender.'<br><br>';
             return $info;
+        }
+    }
+
+    /* Constructor and Destructor are methods. Constructors run right before we create an object.
+    Destructors Right after we are done creating an object.  */
+
+    class PersonX{
+        private $name;
+        private $age;
+        private $gender;
+
+        public function __construct($name,$age,$gender){
+            $this->name = $name;
+            $this->age = $age;
+            $this->gender = $gender;
+        }
+        public function showPersonInfo(){
+            $info = '<br>'.$this->name.'<br>'.$this->age.'<br>'.$this->gender.'<br><br>';
+            return $info;
+        }
+        public function __destruct(){ // Destructor Destroys an instance/ object.
+            echo "Destroyed"; 
         }
     }
 ?>
